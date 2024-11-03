420d3d51bbec73d8f1500b868bfd9d762ace80b1
Chandan Kumar <chandankrr.91@gmail.com>
2024-11-04 01:25:23 +0530

    Add bento grid for landing page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,11 +13,11 @@ export const Navbar = () => {
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-16 text-lg items-center justify-between">
-          <Link href="/" className="flex z-40 font-semibold">
+          <Link href="/" className="flex z-40 font-semibold text-xl">
             Ping<span className="text-brand-700">Panda</span>
           </Link>
 
-          <div className="h-full flex items-center space-x-4">
+          <div className="h-full flex items-center space-x-1 sm:space-x-4">
             {user ? (
               <>
                 <SignOutButton>
